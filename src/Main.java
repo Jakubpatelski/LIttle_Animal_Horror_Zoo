77e21e05a2a3b23fb589a4921a5e131d20125e10
Jakubpatelski <jakubpatelski24@gmail.com>
2023-02-25 16:10:30 +0100

    small update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Animal rabbit = new Animal("Rabbit", 2,2);
+        Animal rabbit = new Animal("Rabbit", 1,1);
         Animal snake = new Animal("Snake", 9, 9);
         Animal animal = new Animal();
         animal.game(rabbit, snake);
